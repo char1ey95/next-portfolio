16e0dc8d551eb0a52ef91169da841a28fad4688d
root <root@Char1ey>
2023-07-30 17:43:02 +0900

    Feat: 메인 페이지 수정 중

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 const Home = () => {
 
   return (
-    <section className="flex min-h-screen flex-column items-center text-gray-600 body-font">
+    <section className="flex flex-col items-center min-h-screen text-gray-600 body-font">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            안녕하세요 신입 개발자 강찬수입니다!
+            안녕하세요,
             <br className="hidden lg:inline-block" />
-            readymade gluten
+            신입 개발자 강찬수입니다!
           </h1>
           <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
           <div className="flex justify-center">
