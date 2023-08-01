a8394919670dd41bbdd7073649f6ff49a013f4cf
char1ey <ckstn410@gmail.com>
2023-08-01 19:38:05 +0900

    Feat: 다크모드 테마 버튼 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from "@/components/header/header"
 import Footer from "@/components/footer/footer"
+import Providers from "./providers"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
