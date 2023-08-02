95869aa749d26b91ebfde95df75eff73f2c517c3
char1ey <ckstn410@gmail.com>
2023-08-02 18:19:50 +0900

    Feat: 환경변수 설정 및 데이터 패칭

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,41 @@
-const Projects = () => {
+import { DATABASE_ID, TOKEN } from "@/config";
 
+const getData = async () => {
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'Notion-Version': '2022-06-28',
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        },
+        body: JSON.stringify({ page_size: 100 })
+    };
+
+    const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+const Projects = async () => {
+    const response = await getData()
+
+    const projectNames = response.results.map((v: any) => (
+        v.properties.이름.title
+    ))
+
+    console.log(1)
     return (
         <div>
-            Hello Projects
+            총 프로젝트 : {
+                projectNames.map((v: any) => <div>
+                    {v[0].text.content}
+                </div>)
+            }
         </div>
     )
 }
